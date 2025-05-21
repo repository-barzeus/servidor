ed6f43564bdc80708b3f64bf43ba09c61bd4ff23
Douglas <github.barzeus@outlook.com.br>
2025-05-21 13:51:14 -0300

    initial: commit inicial do projeto

---
+++ src/main/java/br/com/douglasdreer/Barzeus/BarzeusApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglasdreer.Barzeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarzeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarzeusApplication.class, args);
+	}
+
+}
